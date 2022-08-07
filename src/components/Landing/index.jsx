3e4625c3857c148447dc7b146b95d74f618fa75a
Omkaar Shenoy <omkaarshenoyos@gmail.com>
2022-08-07 00:28:45 -0400

    changes

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -16,36 +16,32 @@ const Landing = () => {
       <div className="container home-page">
         <div className="text-zone">
 
-          <div className="c1"><div class="type-1"> <h1>Hey!</h1><br></br></div></div>
-          <div className="c2"><div class="type-2"><h1> I'm Omkaar Shenoy. </h1><br></br></div></div>
-          <div className="c3"><div class="type-3">Student | Developer</div></div>
+          <div className="c1"><div className="type-1"> <h1>Hey!</h1><br></br></div></div>
+          <div className="c2"><div className="type-2"><h1> I'm Omkaar Shenoy. </h1><br></br></div></div>
+          <div className="c3"><div className="type-3">Student | Developer</div></div>
+                    
+          <div className="shareone">
           
+            <div className="wrapper">
 
+              <div className="social-container">
 
-
-
-          <div class="shareone">
-          
-            <div class="wrapper">
-
-              <div class="social-container">
-
-                <ul class="social-icons">
-                  <li class="icon email">
-                    <span class="tooltip">Email</span>
-                    <span> <a href="mailto:omkaarshenoyos@gmail.com" target="_blank" rel="noopener noreferrer"><i class="fa fa-at" style={{ left: '14px', top: '13px' }}></i></a></span>
+                <ul className="social-icons">
+                  <li className="icon email">
+                    <span className="tooltip">Email</span>
+                    <span> <a href="mailto:omkaarshenoyos@gmail.com" target="_blank" rel="noopener noreferrer"><i className="fa fa-at" style={{ left: '14px', top: '13px' }}></i></a></span>
                   </li>
-                  <li class="icon instagram">
-                    <span class="tooltip">Instagram</span>
-                    <span> <a href="https://www.instagram.com/omkrshny/" target="_blank" rel="noopener noreferrer"><i class="fa fa-instagram" style={{ left: '14px', top: '13px' }}></i></a></span>
+                  <li className="icon instagram">
+                    <span className="tooltip">Instagram</span>
+                    <span> <a href="https://www.instagram.com/omkrshny/" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram" style={{ left: '14px', top: '13px' }}></i></a></span>
                   </li>
-                  <li class="icon linkedin">
-                    <span class="tooltip">Linkedin</span>
-                    <span> <a href="https://www.linkedin.com/in/omkaarshenoy/"><i class="fa fa-linkedin" style={{ left: '14px', top: '13px' }} target="_blank" rel="noopener noreferrer"></i></a></span>
+                  <li className="icon linkedin">
+                    <span className="tooltip">Linkedin</span>
+                    <span> <a href="https://www.linkedin.com/in/omkaarshenoy/"><i className="fa fa-linkedin" style={{ left: '14px', top: '13px' }} target="_blank" rel="noopener noreferrer"></i></a></span>
                   </li>
-                  <li class="icon github">
-                    <span class="tooltip">Github</span>
-                    <span> <a href="https://www.github.com/OmkaarShenoy/"><i class="fa fa-github" style={{ left: '14px', top: '13px' }} target="_blank" rel="noopener noreferrer"></i></a></span>
+                  <li className="icon github">
+                    <span className="tooltip">Github</span>
+                    <span> <a href="https://www.github.com/OmkaarShenoy/"><i className="fa fa-github" style={{ left: '14px', top: '13px' }} target="_blank" rel="noopener noreferrer"></i></a></span>
                     
                   </li>
                 </ul>
@@ -53,10 +49,11 @@ const Landing = () => {
               </div>
             </div>
           </div>
-          <div class="resume"><a href="https://github.com/OmkaarShenoy/Resume/raw/master/main.pdf" download="renamed.pdf"> Resume <i class="fa fa-download" style={{fontSize: '25px'}} rel="noopener noreferrer"></i></a></div>
+          <div className="resume"><a href="https://github.com/OmkaarShenoy/Resume/raw/master/main.pdf" download="renamed.pdf"> Resume <i className="fa fa-download" style={{fontSize: '25px'}} rel="noopener noreferrer"></i></a></div>
 
         </div>
-        <div class="scroll"><Link  to="experiences" spy={true} smooth={true}><Mouse  width="85px "fill="#fff" /></Link></div>
+        {/*<div className="scroll"><Link  to="experiences" spy={true} smooth={true}><Mouse  width="85px "fill="#fff" /></Link></div>*/}
+
 
       </div>
       <Loader type="pacman" />
