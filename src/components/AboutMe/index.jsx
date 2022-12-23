8de304943ad5c470dbecff1a10754fc14c509696
Omkaar Shenoy <omkaarshenoyos@gmail.com>
2022-12-22 18:29:26 -0800

    changes

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -6,16 +6,93 @@ const Aboutme = () => {
       <div className="container about">
         <div className="text-zone">
           <h1>About me!</h1>
-          <h3>My name is <span style={{color:'white', fontWidth:'900px  '}}>Omkaar Shenoy</span> and I am a <span style={{color:'white', fontWidth:'900px  '}}>computer science</span> student at Arizona State University with a minor in <span style={{color:'white', fontWidth:'900px  '}}>Statistics</span>. My focus is on <span style={{color:'white', fontWidth:'900px  '}}>software engineering, data analysis, and machine learning</span>, and I have also participated in various outside-of-class projects and organizations. I am <span style={{color:'white', fontWidth:'900px  '}}>passionate about creating innovative solutions</span> that make a positive impact on society and am always looking for new opportunities to grow and learn.  
-
-<br/><br/>
-
-As a Computer Science student, I <span style={{color:'white', fontWidth:'900px  '}}>enjoy building software </span> that makes lives easier.  Whether it's a new app that streamlines a tedious process or a website that provides valuable information clearly and concisely,  I believe <span style={{color:'white', fontWidth:'900px  '}}>user experience is the most important part of a product </span>. I love the challenge of creating something useful and user-friendly. I have always been fascinated by the <span style={{color:'white', fontWidth:'900px  '}}>power of data </span>and its ability to help us understand the world around us. That's why <span style={{color:'white', fontWidth:'900px  '}}>I love data visualization </span>and the art of turning complex data sets into something easy for people to understand. <br/><br/>
-
-
-This past summer, I had the privilege of <span style={{color:'white', fontWidth:'900px  '}}>interning at National Public Radio </span>, a fantastic experience that enhanced my expertise and abilities. It was an incredible experience that <span style={{color:'white', fontWidth:'900px  '}}>solidified my love for all things tech</span>. Working alongside experienced professionals and contributing to high-impact projects gave me <span style={{color:'white', fontWidth:'900px  '}}>valuable insights into the industry </span>and helped me to develop my skills and knowledge even further. I am eager to continue learning and developing my skills as a computer science professional, and <span style={{color:'white', fontWidth:'900px  '}}>I look forward to all the exciting opportunities </span> that lie ahead.
-<br/><br/>
-When I'm not coding or analyzing data, you can find me <span style={{color:'white', fontWidth:'900px  '}}>chilling with friends</span>, sampling new <span style={{color:'white', fontWidth:'900px  '}}>coffee shops </span>, and <span style={{color:'white', fontWidth:'900px  '}}>spending time outdoors</span>, such as going on hikes or simply appreciating the Arizona sunshine.</h3>
+          <h3>
+            My name is{' '}
+            <span style={{ color: 'white', fontWidth: '900px  ' }}>
+              Omkaar Shenoy
+            </span>{' '}
+            and I am a{' '}
+            <span style={{ color: 'white', fontWidth: '900px  ' }}>
+              computer science
+            </span>{' '}
+            student at Arizona State University with a minor in{' '}
+            <span style={{ color: 'white', fontWidth: '900px  ' }}>
+              Statistics
+            </span>
+            . My focus is on{' '}
+            <span style={{ color: 'white', fontWidth: '900px  ' }}>
+              software engineering, data analysis, and machine learning
+            </span>
+            , and I have also participated in various outside-of-class projects
+            and organizations. I am{' '}
+            <span style={{ color: 'white', fontWidth: '900px  ' }}>
+              passionate about creating innovative solutions
+            </span>{' '}
+            that make a positive impact on society and am always looking for new
+            opportunities to grow and learn.
+            <br />
+            <br />
+            As a Computer Science student, I{' '}
+            <span style={{ color: 'white', fontWidth: '900px  ' }}>
+              enjoy building software{' '}
+            </span>{' '}
+            that makes lives easier. Whether it's a new app that streamlines a
+            tedious process or a website that provides valuable information
+            clearly and concisely, I believe{' '}
+            <span style={{ color: 'white', fontWidth: '900px  ' }}>
+              user experience is the most important part of a product{' '}
+            </span>
+            . I love the challenge of creating something useful and
+            user-friendly. I have always been fascinated by the{' '}
+            <span style={{ color: 'white', fontWidth: '900px  ' }}>
+              power of data{' '}
+            </span>
+            and its ability to help us understand the world around us. That's
+            why{' '}
+            <span style={{ color: 'white', fontWidth: '900px  ' }}>
+              I love data visualization{' '}
+            </span>
+            and the art of turning complex data sets into something easy for
+            people to understand. <br />
+            <br />
+            This past summer, I had the privilege of{' '}
+            <span style={{ color: 'white', fontWidth: '900px  ' }}>
+              interning at National Public Radio{' '}
+            </span>
+            , a fantastic experience that enhanced my expertise and abilities.
+            It was an incredible experience that{' '}
+            <span style={{ color: 'white', fontWidth: '900px  ' }}>
+              solidified my love for all things tech
+            </span>
+            . Working alongside experienced professionals and contributing to
+            high-impact projects gave me{' '}
+            <span style={{ color: 'white', fontWidth: '900px  ' }}>
+              valuable insights into the industry{' '}
+            </span>
+            and helped me to develop my skills and knowledge even further. I am
+            eager to continue learning and developing my skills as a computer
+            science professional, and{' '}
+            <span style={{ color: 'white', fontWidth: '900px  ' }}>
+              I look forward to all the exciting opportunities{' '}
+            </span>{' '}
+            that lie ahead.
+            <br />
+            <br />
+            When I'm not coding or analyzing data, you can find me{' '}
+            <span style={{ color: 'white', fontWidth: '900px  ' }}>
+              chilling with friends
+            </span>
+            , sampling new{' '}
+            <span style={{ color: 'white', fontWidth: '900px  ' }}>
+              coffee shops{' '}
+            </span>
+            , and{' '}
+            <span style={{ color: 'white', fontWidth: '900px  ' }}>
+              spending time outdoors
+            </span>
+            , such as going on hikes or simply appreciating the Arizona
+            sunshine.
+          </h3>
         </div>
       </div>
     </>
